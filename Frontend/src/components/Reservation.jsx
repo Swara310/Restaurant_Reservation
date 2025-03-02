5908ef3396eb2513e88ac9cd66659357e1d6b7b5
unknown <9146939196>
2025-03-02 20:41:50 +0530

    Updated .gitignore files and modified backend app.js

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "https://restaurant-reservation-i4zt.onrender.com",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
